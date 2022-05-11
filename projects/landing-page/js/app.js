85436082dd9d6a30767c5d26f3a7388631387567
Byron Meals <bmeals87@gmail.com>
2022-05-11 10:13:58 -0600

    Changed viewport check to only look at the top edge for activity

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -123,13 +123,16 @@ console.log(isInViewport);*/
 
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
-  return (
+  /*return (
         rect.top >= 0 &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
-    );
+    );*/
+  return (
+        rect.top >= 0 &&
+        rect.left >= 0 );
 }
 
 function checkActiveSection() {
