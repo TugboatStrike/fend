8f4f4b64f5f333eb06c6459db8a06554e25bc88f
Byron Meals <bmeals87@gmail.com>
2022-05-10 21:07:04 -0600

    Added scroll checking for section is in viewport

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -32,10 +32,10 @@ const alignToBot = {behavior: "smooth", block: "end", inline: "nearest"}
 function navButton(element) {
   const newLi = document.createElement('li');
   newLi.append(element.dataset.nav);/* Add element dataset text as li display text*/
-  newLi.classList.add("menu__link");
+  newLi.classList.add("menu__link");/* Add class "menu__link" to li*/
   newLi.addEventListener('click', scrollToSection);
-  newLi.dataset.navId = element.id;
-  newLi.dataset.nav = element.dataset.nav;
+  newLi.dataset.navId = element.id; /* Add dataset of section ID*/
+  newLi.dataset.nav = element.dataset.nav;/* Add nav text as prop to li*/
   return newLi;
   /*element.scrollIntoView(alignToBot);*/
 }/* creating nav buttons for the sectionList
@@ -94,6 +94,44 @@ testClick.addEventListener('click', function testingLog(){
   console.log('testing clicked');
 })
 
+
+const box = document.querySelector('#section2');
+const rect = box.getBoundingClientRect();
+
+/*
+const isInViewport = rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+console.log(isInViewport);*/
+
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
+const activeText = "your-active-class";
+
+document.addEventListener('scroll', function() {
+  console.log('---start---');
+  for (const section of sectionList) {
+    if (isInViewport(section)) {
+      section.classList.add(activeText)
+    } else {
+      section.classList.remove(activeText)
+    }
+
+    console.log(isInViewport(section));
+  }
+  console.log('---end---');
+})
 // Add class 'active' to section when near top of viewport
 
 
